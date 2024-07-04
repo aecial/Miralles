2799e551a055f35268ad66f456f0b275a590f138
Teddy Pascual <teddylayson2000@gmail.com>
2024-07-04 20:38:06 +0800

    add basic carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#C69B6D",
+
+          secondary: "#ff00ff",
+
+          accent: "#00ffff",
+
+          neutral: "#ff00ff",
+
+          "base-100": "#ff00ff",
+
+          info: "#0000ff",
+
+          success: "#00ff00",
+
+          warning: "#00ff00",
+
+          error: "#ff0000",
+
+          clean: "#FFFFFF",
+        },
+      },
+    ],
+  },
   content: ["./*.html"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("preline/plugin")],
 };
