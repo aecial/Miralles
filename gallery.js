ab1e5cb792b393f8928a40eda32433e96667c19a
Teddy Pascual <teddylayson2000@gmail.com>
2024-07-09 16:48:56 +0800

    add responsive navbar

--- gallery.js
+++ gallery.js
@@ -64,3 +64,18 @@ prevBtn.addEventListener("click", () => {
     photoCollection[currentIndex].length;
   nowShowing.src = photoCollection[currentIndex][currentPhotoIndex];
 });
+
+const hamburgerBtn = document.getElementById("myHamburger");
+const navbarDiv = document.getElementById("navbarDiv");
+function clicked() {
+  if (hamburgerBtn.classList.contains("swap-active")) {
+    hamburgerBtn.classList.remove("swap-active");
+    navbarDiv.classList.remove("flex");
+
+    navbarDiv.classList.add("hidden");
+  } else {
+    hamburgerBtn.classList.add("swap-active");
+    navbarDiv.classList.remove("hidden");
+    navbarDiv.classList.add("flex");
+  }
+}
